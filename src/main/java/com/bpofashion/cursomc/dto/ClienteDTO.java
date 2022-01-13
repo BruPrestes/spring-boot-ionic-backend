47fcb6756a1b88f1060ed637e99a00cd4c6f332b
BruPrestes <brunnoprestes@gmail.com>
2022-01-12 22:51:57 -0300

    Validacao customizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/bpofashion/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/bpofashion/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.bpofashion.cursomc.domain.Cliente;
+import com.bpofashion.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
